95f42b6d4dbf71ae71565d23992264d286c0b949
Merle <maximerle@gmail.com>
2016-09-21 16:41:16 +0200

    page valider.php (validation inscription)+Inscription.php

--- inscription.php
+++ inscription.php
@@ -4,10 +4,13 @@ include('header.html');
 ?>
 
 <div class="bg1">
-<div class="container-fluid">
-	<div class="row">
-		<div class="formulaire col-lg-offset-4 col-lg-6 col-md-offset-4 col-md-6 col-sm-offset-2 col-sm-5 col-xs-offset-1 col-xs-6">
-			<form action="resultats.php" method="post">
+	<div class="container-fluid">
+		<div class="row">
+
+
+
+			<div class="formulaire col-lg-offset-1 col-lg-4  col-md-6 col-sm-5 col-xs-12">
+				<form action="valider.php" method="post">
 
 				<p class="text-formu">Votre nom <br/>
 				    <input type="text" name="nom"  size="40" class="requis"/></p>
@@ -21,16 +24,23 @@ include('header.html');
 				<p class="btn-envoyer"><input type="submit" value="Envoyer" class="envoyer"/></p>
 			
 
-			</form>
+				</form>
 
+			</div>
+
+			<div class="box-formulaire  col-lg-offset-2 col-lg-5  col-md-6  col-sm-3  col-xs-12">
+			<h1>Incrivez-vous vite pour participer à notre grand tirage au sort ! </h1>
+			<p>Il vous suffit de rentrer votre Nom, Prenom et votre numero de téléphone pour valider votre iscription ! </p>
+			
+			</div>
 		</div>
 	</div>
 </div>
-</div>
+
 <div class="container-fluid">
 	<div class="row">
 		<section class="col-lg-offset-4 col-lg-6 col-md-offset-4 col-md-6 col-sm-offset-2 col-sm-5 col-xs-offset-1 col-xs-6">
-			<img src="contenu/images/compte.jpg">
+			
 		</section>
 	</div>
 </div>
---
+++ valider.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+include('header.html');
+?>
+<div class="">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="validé col-lg-5 ">
+				<h1 >Votre Inscription a bien étée prise en compte ! </h1>
+				<p>Restez conncté sur notre site afin de suivre le déroulé du tirage au sort ! 
+				Les résultats seront en ligne le 1/10/2016 à 00h00 ! </p>
+			</div>
+			
+		</div>	
+
+	</div>
+</div>
+
+<?php
+session_start();
+include('footer.html');
+?>
