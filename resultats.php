26c531fde4a4a10c1ee28c9c59a67c4fae658985
quentin78 <quentin29norm@gmail.com>
2016-09-22 14:31:57 +0200

    bien vu max nrv la banniere

--- resultats.php
+++ resultats.php
@@ -3,15 +3,22 @@ session_start();
 include('header.html');
 ?>
 
-<div class="container-fluid">
-	<div class="bloc row col-lg-6">
-
-
-
-
-
-	</div>
-</div>
+<aside class="profile-card">
+    <header>
+      <a><img src="https://github.com/daliannyvieira/daliannyvieira.github.io/blob/master/img/profile.jpg?raw=true"></a>
+      <h1>Dalianny Vieira</h1>
+      <h2>Hello!</h2>
+    </header>
+    <div class="profile-bio">
+      <p>You can call me Dali. I'm a brazilian <strong>frontend web developer, illustrator, and designer</strong>. Making some cool things with coffee and code.</p>
+      <ul class="social-icons list-unstyled list-inline">
+        <li><a href="https://www.behance.net/daliannyvieira"><i class="fa fa-behance"></i></a></li>
+        <li><a href="https://codepen.io/daliannyvieira/"><i class="fa fa-codepen"></i></a></li>
+        <li><a href="https://github.com/daliannyvieira/"><i class="fa fa-github"></i></a></li>
+        <li><a href="https://twitter.com/daliannyvieira"><i class="fa fa-twitter"></i></a></li>
+      </ul>
+    </div>
+  </aside>
 
 
 
